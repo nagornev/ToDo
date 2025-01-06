8913a855cbc7b9756a3340a4474d485886f0c222
nagornev <pavel.maksimovich.nagornev@gmail.com>
2025-01-06 13:34:12 +0300

    Created test project for entry microservice.

---
+++ ToDo.Microservices/ToDo.Microservices.Entries/ToDo.Microservices.Entries.Tests/Mock/EntryContextMock.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Microservices.Entries.Database.Contexts;
+using ToDo.Microservices.Entries.Database.Extensions;
+using ToDo.Microservices.Entries.Database.Entities;
+using ToDo.Microservices.Entries.Domain.Models;
+
+namespace ToDo.Microservices.Entries.Tests.Mock
+{
+    public class EntryContextMock : EntryContext
+    {
+        public IReadOnlyDictionary<User, IEnumerable<Entry>> Data { get; }
+
+        public EntryContextMock(string dbName, Func<IReadOnlyDictionary<User, IEnumerable<Entry>>> data = null)
+                    : base(new DbContextOptionsBuilder<EntryContext>()
+                                    .UseSqlite($"DataSource=file:{dbName}?mode=memory&cache=shared")
+                                    .Options)
+        {
+            Database.OpenConnection();
+            Database.EnsureCreated();
+
+            Data = data?.Invoke() ?? new Dictionary<User, IEnumerable<Entry>>();
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+
+            foreach (User user in Data.Keys)
+            {
+                UserEntity userEntity = user.GetEntity();
+
+                Users.Add(userEntity);
+
+                foreach (Entry entry in Data[user])
+                {
+                    EntryEntity entryEntity = entry.GetEntity(user.Id);
+
+                    Entries.Add(entryEntity);
+                }
+
+                SaveChanges();
+            }
+        }
+    }
+}
---
+++ ToDo.Microservices/ToDo.Microservices.Entries/ToDo.Microservices.Entries.Tests/Units/EntryRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using ToDo.Microservices.Entries.Domain.Models;
+using ToDo.Microservices.Entries.Tests.Mock;
+
+namespace ToDo.Microservices.Entries.Tests.Units
+{
+    public class EntryRepositoryTests
+    {
+        private EntryContextMock GetEntryContextMock(Func<IReadOnlyDictionary<User, IEnumerable<Entry>>> startContext = null)
+        {
+            return new EntryContextMock($"{Guid.NewGuid()}", startContext);
+        }
+    }
+}
